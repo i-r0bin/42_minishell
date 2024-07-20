b1c5d185c1deaa57aef328d2eb382eb9e62976dd
ensteum <pezzullopietro04@gmail.com>
2024-07-20 20:08:10 +0200

    Permission fix

--- src/redir_utils.c
+++ src/redir_utils.c
@@ -6,62 +6,86 @@ void	append_redirection(t_data *data, int index);
 void	exec_here_documents(t_data *data, int index);
 void	remove_redir_args(t_data *data, int index);
 
-void	input_redirection(t_data *data, int index)
-{
-	int	fd;
-	int	prev_fd;
+int check_file_permissions(char *file, int flags) {
+    int fd;
 
-	prev_fd = dup(0);
-	fd = open(data->args[index + 1], O_RDONLY);
-	dup2(fd, 0);
-	close(fd);
-	free(data->args[index]);
-	data->args[index] = ft_calloc(1, 1);
-	free(data->args[index + 1]);
-	data->args[index + 1] = ft_calloc(1, 1);
-	remove_null_args(data);
-	exec_cmd(data);
-	dup2(prev_fd, 0);
-	close(prev_fd);
+    fd = open(file, flags, 0644);
+    if (fd < 0) {
+
+        perror("Permission denied");
+        return -1;
+    }
+    close(fd);
+    return 0;
 }
 
-void	output_redirection(t_data *data, int index)
-{
-	int	fd;
-	int	prev_fd;
 
-	prev_fd = dup(1);
-	fd = open(data->args[index + 1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
-	dup2(fd, 1);
-	close(fd);
-	free(data->args[index]);
-	data->args[index] = ft_calloc(1, 1);
-	free(data->args[index + 1]);
-	data->args[index + 1] = ft_calloc(1, 1);
-	remove_null_args(data);
-	exec_cmd(data);
-	dup2(prev_fd, 1);
-	close(prev_fd);
+void input_redirection(t_data *data, int index) {
+    int fd;
+    int prev_fd;
+
+    prev_fd = dup(0);
+    fd = open(data->args[index + 1], O_RDONLY);
+    dup2(fd, 0);
+    close(fd);
+    free(data->args[index]);
+    data->args[index] = ft_calloc(1, 1);
+    free(data->args[index + 1]);
+    data->args[index + 1] = ft_calloc(1, 1);
+    remove_null_args(data);
+    exec_cmd(data);
+    dup2(prev_fd, 0);
+    close(prev_fd);
 }
 
-void	append_redirection(t_data *data, int index)
-{
-	int	fd;
-	int	prev_fd;
+void output_redirection(t_data *data, int index) {
+    int fd;
+    int prev_fd;
 
-	prev_fd = dup(1);
-	fd = open(data->args[index + 1], O_WRONLY | O_CREAT | O_APPEND, 0644);
-	dup2(fd, 1);
-	close(fd);
-	free(data->args[index]);
-	data->args[index] = ft_calloc(1, 1);
-	free(data->args[index + 1]);
-	data->args[index + 1] = ft_calloc(1, 1);
-	remove_null_args(data);
-	exec_cmd(data);
-	dup2(prev_fd, 1);
-	close(prev_fd);
+    if (check_file_permissions(data->args[index + 1], O_WRONLY | O_CREAT | O_TRUNC) < 0)
+	{
+		data->status = 1;
+        return;
+	}
+
+    prev_fd = dup(1);
+    fd = open(data->args[index + 1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    dup2(fd, 1);
+    close(fd);
+    free(data->args[index]);
+    data->args[index] = ft_calloc(1, 1);
+    free(data->args[index + 1]);
+    data->args[index + 1] = ft_calloc(1, 1);
+    remove_null_args(data);
+    exec_cmd(data);
+    dup2(prev_fd, 1);
+    close(prev_fd);
 }
+
+void append_redirection(t_data *data, int index) {
+    int fd;
+    int prev_fd;
+
+    if (check_file_permissions(data->args[index + 1], O_WRONLY | O_CREAT | O_APPEND) < 0)
+	{
+		data->status = 1;
+        return;
+	}
+
+    prev_fd = dup(1);
+    fd = open(data->args[index + 1], O_WRONLY | O_CREAT | O_APPEND, 0644);
+    dup2(fd, 1);
+    close(fd);
+    free(data->args[index]);
+    data->args[index] = ft_calloc(1, 1);
+    free(data->args[index + 1]);
+    data->args[index + 1] = ft_calloc(1, 1);
+    remove_null_args(data);
+    exec_cmd(data);
+    dup2(prev_fd, 1);
+    close(prev_fd);
+}
+
 
 void	exec_here_documents(t_data *data, int index)
 {
